fd94955c3c403f429e49b0db695efb5cde350db9
Anderson Campos de Mendonça <andersoncampos.m@gmail.com>
2013-05-21 14:46:28 -0300

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,52 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   describe "signup page" do
+    
     before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with all blank fields" do
+      before { click_button submit }
+
+      it "should raise error message" do
+        assert page.has_content?('Password is too short')
+        assert page.has_content?('Email is invalid')
+      end
+    end    
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      it "should redirect to profile page" do
+        click_button submit
+
+        assert page.has_content?("Example User")
+      end
+    end
   end
 end
